7dc907ae3f937d206766789ee1f0635fa0f688dc
AYLazcano <0370102@cegepmontpetit.ca>
2024-02-08 22:40:50 -0500

    FCT: Tiroir de navigation

--- app/src/main/java/com/example/tp1clientandroid/MainActivity.java
+++ app/src/main/java/com/example/tp1clientandroid/MainActivity.java
@@ -1,14 +1,109 @@
 package com.example.tp1clientandroid;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.tp1clientandroid.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // Recherche des éléments de la vue
+        NavigationView nv = binding.navView;
+        DrawerLayout dLayout = binding.drawerLayout;
+
+        // Affichage de l'icône de menu et interaction
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Changement automatique de l'icône de menu et du titre avec l'action du tiroir
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, dLayout, R.string.drawer_open,R.string.drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(R.string.drawer_open);
+                Toast.makeText(MainActivity.this, "Ouvert", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(R.string.drawer_close);
+                Toast.makeText(MainActivity.this, "Fermé", Toast.LENGTH_SHORT).show();
+            }
+
+        };
+
+        dLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent i;
+
+                if (item.getItemId() == R.id.nav_home) {
+                    i =  new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(i);
+                    return true;
+
+                } else if (item.getItemId() == R.id.nav_add_task) {
+                    i = new Intent(MainActivity.this, TaskCreationActivity.class);
+                    startActivity(i);
+                    return true;
+
+                } else if (item.getItemId() == R.id.nav_logout) {
+                    Toast.makeText(MainActivity.this, R.string.logout, Toast.LENGTH_SHORT).show();
+                    Log.d("MainActivity", "Utilisateur déconnecté");
+                    i =  new Intent(MainActivity.this, ConnexionActivity.class);
+                    startActivity(i);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    // Interaction avec l'icône de menu lorsque l'utilisateur appuie dessus
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Conservation de l'état de l'icône de menu lorsqu'il y a un changement d'orientation
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    // Conservation de l'état de l'icône de menu lorsqu'il y a un changement d'orientation
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+        super.onConfigurationChanged(newConfig);
     }
 }
